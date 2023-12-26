c796839f72ab645189f32065ac4b4b41cdc733bc
bumzacktrara <bumzacktrara@gmail.com>
2023-12-26 21:33:48 +0100

    examples

--- rust/common/examples/extract_rgb.rs
+++ rust/common/examples/extract_rgb.rs
@@ -10,20 +10,27 @@ fn main() {
     let full_path = format!("{}/{}.rs", env!("CARGO_MANIFEST_DIR"), "colors");
     let mut file = File::create(full_path).expect("cant create file");
 
-    file.write(format!("pub const PALETTE : Vec<Color> = vec! [   \n", ).as_ref())
+    let a = file
+        .write(
+            "pub const PALETTE : Vec<Color> = vec! [   \n"
+                .to_string()
+                .as_ref(),
+        )
         .expect("Unable to write pixel to file");
 
     colors.iter().for_each(|c| {
-        file.write(
-            format!(
-                "Color {}  r:   {}, g: {},  b: {} {},   \n ",
-                "{", c.r, c.g, c.b, "}"
-            )
+        let b = file
+            .write(
+                format!(
+                    "Color {}  r:   {}, g: {},  b: {} {},   \n ",
+                    "{", c.r, c.g, c.b, "}"
+                )
                 .as_ref(),
-        )
+            )
             .expect("Unable to write pixel to file");
     });
 
-    file.write(format!("];   \n", ).as_ref())
+    let c = file
+        .write("];   \n".to_string().as_ref())
         .expect("Unable to write pixel to file");
 }
--- rust/readme.md
+++ rust/readme.md
@@ -1,6 +1,5 @@
 # FractalThingi
 
-
 ## see here for the centers of the different special fuigures
 
 https://mandel.gart.nz/#/
